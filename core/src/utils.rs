528a80e79898249c57796b90e1c1a4d68e03c97e
Rxann <80224521+Rxann@users.noreply.github.com>
2022-05-24 19:44:47 -0400

    Add some more progress

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -24,6 +24,26 @@ pub(crate) fn extract_digits(s: &str) -> (&str, &str) {
 pub(crate) fn extract_whitespace(s: &str) -> (&str, &str) {
     take_while(|c| c == ' ', s)
 }
+pub(crate) fn extract_ident(s: &str) -> (&str, &str) {
+    let input_starts_with_alphabetic = s
+        .chars()
+        .next()
+        .map(|c| c.is_ascii_alphabetic())
+        .unwrap_or(false);
+
+    if input_starts_with_alphabetic {
+        take_while(|c| c.is_ascii_alphanumeric(), s)
+    } else {
+        (s, "")
+    }
+}
+pub(crate) fn tag<'a, 'b>(starting_text: &'a str, s: &'b str) -> &'b str {
+    if s.starts_with(starting_text) {
+        &s[starting_text.len()..]
+    } else {
+        panic!("expected {}", starting_text);
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -69,5 +89,17 @@ mod tests {
     #[test]
     fn extract_spaces() {
         assert_eq!(extract_whitespace("    1"), ("1", "    "));
+    }
+    #[test]
+    fn extract_alphanumeric_ident() {
+        assert_eq!(extract_ident("foobar1()"), ("()", "foobar1"));
+    }
+    #[test]
+    fn cannot_extract_ident_beginning_with_number() {
+        assert_eq!(extract_ident("123abc"), ("123abc", ""));
+    }
+    #[test]
+    fn tag_word() {
+        assert_eq!(tag("let", "let a"), " a");
     }
 }
